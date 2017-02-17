1ae10d7d209371a107b96379079e0c755db60a5d
Walter Litwinczyk <wtl4112@rit.edu>
2017-02-16 22:02:19 -0800

    Multiple objects

--- src/main.ml
+++ src/main.ml
@@ -4,12 +4,10 @@ open Vec
 open Ray
 open Objects
 
-let color ray : Vec.t =
-    let t = hit_sphere {x=0.;y=0.;z= -1.} 0.5 ray in
-    if t > 0. then
-        let n = norm (pos ray t) -. {x=0.;y=0.;z= -1.} in
-        s_mult 0.5 {x=n.x + 1.; y=n.y + 1.; z=n.z + 1.}
-    else
+let color ray objs : Vec.t =
+    match hit_many ray 0.0 max_float objs with
+    | Some {t;p;normal=n} -> s_mult 0.5 {x=n.x + 1.; y=n.y + 1.; z=n.z + 1.}
+    | None ->
         let n_dir = norm ray.dir in
         let t : float = 0.5 * (n_dir.y + 1.0) in
         (add (s_mult (1.0 - t) (Vec.mk 1.0)) (s_mult t {x=0.5;y=0.7;z=1.0}))
@@ -23,13 +21,16 @@ let _ =
     and horizontal = {x=4.;y=0.;z=0.}
     and vertical = {x=0.;y=2.;z=0.}
     and origin = Vec.zero in
+    let s1 = Sphere ({x=0.;y=0.;z= -1.}, 0.5)
+    and s2 = Sphere ({x=0.;y= -100.5;z= -1.}, 100.) in
+    let objs = [s1;s2] in
     iter_image (fun x y _ ->
             let u = float x / float nx
             and v = float y / float ny in
             let u_vec = s_mult u horizontal
             and v_vec = s_mult v vertical in
             let ray = Ray.mk origin (add lower_left (add u_vec v_vec)) in
-            let color = vec_to_color (color ray) in
+            let color = vec_to_color (color ray objs) in
             set_pixel img x y color
     ) img;
     write_ppm img "out.ppm"
--- src/objects.ml
+++ src/objects.ml
@@ -1,14 +1,53 @@
 open Vec
 open Ray
 
-let hit_sphere center radius ray : float =
+type hit_record = {
+    t : float;
+    p : Vec.t;
+    normal : Vec.t;
+}
+
+type obj =
+    | Sphere of Vec.t * float
+
+(* hit : (ray : Ray.t) -> (t_min : float) -> (t_max : float) -> hit_record option *)
+
+let hit_sphere center radius ray t_min t_max : hit_record option =
     let oc = sub ray.origin center in
     let a = dot ray.dir ray.dir in
-    let b = 2.0 *. dot oc ray.dir in
+    let b = dot oc ray.dir in
     let c = (dot oc oc) -. (radius *. radius) in
-    let descriminant = b*.b -. 4.0*.a*.c in
-    if descriminant < 0. then
-        -.1.
+    let descriminant = b*.b -. a*.c in
+    if descriminant > 0. then
+        let temp = (-.b -. sqrt(b*.b -. a*.c)) /. a in
+        if temp < t_max && temp > t_min then
+            let t = temp in
+            let p = Ray.pos ray t in
+            let normal = s_div radius (sub p center) in
+            Some {t;p;normal}
+        else begin
+            let temp = (-.b +. sqrt(b*.b-.a*.c)) /. a in
+            if temp < t_max && temp > t_min then
+                let t = temp in
+                let p = Ray.pos ray t in
+                let normal = s_div radius (sub p center) in
+                Some {t;p;normal}
+            else 
+                None
+        end
     else
-        -.b -. sqrt descriminant /. (2.0 *. a)
+        None
+;;
+
+let hit ray t_min t_max = function
+    | Sphere (center, radius) -> hit_sphere center radius ray t_min t_max
+;;
+
+let hit_many ray t_min t_max objs =
+    List.fold_left (fun acc obj ->
+        match acc, hit ray t_min t_max obj with
+        | Some {t=t_old}, (Some {t} as out) -> if t < t_old then out else acc
+        | Some _, None -> acc
+        | None, s -> s
+    ) None objs
 ;;
